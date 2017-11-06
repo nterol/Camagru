524482817f024d99c59d57d0eed6f20933a11094
Edwin <nicolas@MacBook-Pro-de-Edwin.local>
2017-11-06 20:44:08 +0100

    g copoié g pas compris

--- forms/montages.php
+++ forms/montages.php
@@ -27,5 +27,51 @@ else
   $copy = imagecreatetruecolor(640, 480);
 
 imagealphablending($copy, false);
+imagesavealpha($copy, true);
 
+$src = imagecreatefrompng($img);
+
+if (strcmp($img, "../img/diademe.png") == 0 || strcmp($img, "../img/illuminati.png") ==0)
+  imagecopyresized($copy, $src, 0, 0, 0, 0, 240, 180, 1024, 768);
+else
+  imagecopyresized($copy, $src, 0, 0, 0, 0, 640, 480, 1024, 768);
+
+$destination = imagecreatefrompng($montageDir. $iuid. ".png");
+
+$x_src = imagesx($copy);
+$y_src = imagesy($copy);
+$x_dest = imagesx($destination);
+$y_dest = imagesy($destination);
+
+if (strcmp($img, "../img/diademe.png") == 0)
+{
+  $dest_x = 100;
+  $dest_y = 200;
+} else if (strcmp($img, "../img/illuminati.png") == 0) {
+  $dest_x = 180;
+  $dest_y = 0;
+} else {
+  $dest_x = 0;
+  $dest_y = 0;
+}
+
+imagecopymerge_alpha($destination, $copy, $dest_x, $dest_y, 0, 0, $x_src, $y_src, 100);
+
+$success = imagepng($destination, $montageDir.$iuid.".png");
+
+if ($success) {
+  if (($val = add_montage($id, $iuid.".png")) === 0)
+    echo ($iuid.'.png');
+   else
+    echo $val;
+}
+
+function imagecopymerge_alpha($dest, $srcIm, $dest_x, $dest_y, $src_x, $src_y, $src_w, $src_h, $wtf) {
+  $cut = imagecreatetruecolor($src_w, $src_h);
+
+  imagecopy($cut, $dest, 0, 0, $dest_x, $dest_y, $src_w, $src_h);
+  imagecopy($cut, $srcIm, 0, 0, $src_x, $src_y, $src_w, $src_h);
+
+  imagecopymerge($dest, $cut, $dest_x, $dest_y, 0, 0, $src_w, $src_h, $wtf);
+}
  ?>
